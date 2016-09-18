97a1ff53013b8bb721622af13bc58be8f0c259a1
pham186 <binh.phamvan@gmail.com>
2016-09-18 23:42:11 +0700

    #change

--- app/app.js
+++ app/app.js
@@ -6,10 +6,6 @@ var gameapp = angular.module('gameapp', ['ngRoute', 'ngSanitize'])
         templateUrl: 'templates/main.html',
         controller: 'Main'
     })
-    .when('/404.html', {
-        templateUrl: 'templates/detail.html',
-        controller: 'Detail'
-    })
     .when('/:slug.html', {
             templateUrl: 'templates/detail.html',
             controller: 'Detail'
@@ -20,31 +16,72 @@ var gameapp = angular.module('gameapp', ['ngRoute', 'ngSanitize'])
     })
     .otherwise({templateUrl:'templates/404.html'})
 })
-.run(function($rootScope, categories) {
-    $rootScope.categories = categories;
+.factory('GameData', function ($rootScope, $http) {
+    var data = {};
+
+    return {
+        init : function () {
+            $http.get("data/categories.json").then(function(response) {
+                if(angular.isObject(response)) {
+                    $rootScope.categories = response.data;
+                    data.categories = response.data;
+                } else {
+                    return false;
+                }
+            }, function(errorResponse) {
+                return false;
+            });
+            $http.get("data/games.json").then(function(response) {
+                if(angular.isObject(response)) {
+                    data.games = response.data;
+                } else {
+                    return false;
+                }
+            }, function(errorResponse) {
+                return false;
+            });
+            return true;
+        },
+
+        listCategory : function () {
+            return data.categories;
+        },
+
+        getCategory : function (category) {
+            return data.categories;
+        }
+    }
 })
-.controller('Main', function($scope, $http, $routeParams) {
+.run(function($rootScope, GameData) {
+    var init = GameData.init();
+    if(init) {
+        $rootScope.categories = GameData.listCategory();
+        console.log('init ok');
+    } else {
+        console.log('init false');
+    }
+
+})
+.controller('Main', function($rootScope, $scope, $http) {
     $scope.title = 'Game App';
     $scope.welcome = 'Welcome you';
     document.querySelector('title').innerHTML = 'Game App';
 })
-.controller('Category', function($rootScope, $scope, $http, $routeParams, filterFilter) {
+.controller('Category', function($rootScope, $scope, $http, $routeParams, filterFilter, GameData) {
     var category = filterFilter($rootScope.categories, {slug: $routeParams.slug});
-    // console.log(category);
     $scope.title = category[0].title;
     $scope.welcome = 'Welcome you';
     document.querySelector('title').innerHTML = category[0].title;
 })
-.controller('Detail', function($scope, $http, $routeParams) {
-    console.log($routeParams.slug);
-    $http.get('game/' + $routeParams.slug + '/info.json').success(function(res){
-        $scope.item = res;
-    }).error(function (responseError) {
-        $scope.item = {'title' : 'Error', 'desc' : 'Cannot load game data'};
-    });
-    $scope.title = 'Game App';
-    $scope.welcome = 'Welcome you';
-    document.querySelector('title').innerHTML = 'Game App';
+.controller('Detail', function($rootScope, $scope, $http, $routeParams, $location, filterFilter, $sce) {
+    var game = filterFilter($rootScope.games, {slug: $routeParams.slug});
+    if(game != undefined && game.length > 0) {
+        $scope.item = game[0];
+        $scope.iframeSrc = $sce.trustAsResourceUrl('game/' + $routeParams.slug + '/');
+        document.querySelector('title').innerHTML = game[0].title;
+    } else {
+        $location.path('/404.html');
+    }
 })
 .filter('toTrusted', ['$sce', function($sce) {
     return function(text) {
